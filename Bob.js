3ae93880e1401b33773f335b3abeabff98a255fe
sugandha-11 <70713015+sugandha-11@users.noreply.github.com>
2020-11-25 21:07:14 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,27 @@
+class Bob{
+    constructor(x, y,r) {
+      var options = {
+        isStatic :false,
+        restitution : 0.3,
+        friction: 0.5,
+        density:1.2
+
+      }
+      this.x = x;
+      this.y = y;
+      this.r = r;
+      this.body = Bodies.circle (x, y, this.r/2, options);
+
+      World.add(world,this.body);
+    }
+    display(){
+      var pos = this.body.position;
+      push()
+      translate(pos.x, pos.y);
+      rectMode(CENTER);
+      strokeWeight(3);
+      fill(225,0,225);
+      ellipse(0,0,this.r,this.r);
+      pop()
+ }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# rudra_cradle
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
